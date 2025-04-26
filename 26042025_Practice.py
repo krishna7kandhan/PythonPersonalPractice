88f18899ee469a43fec6c15c94bc3db4878bdbba
krishna7kandhan <muthuvijayapriyan@gmail.com>
2025-04-26 15:32:04 +0530

    initial commit

---
+++ 26042025_Practice.py
@@ -0,0 +1,12 @@
+message = 'Hello world !!'
+print(message)
+mesage='practice makes a man perfect!!'
+print(mesage)
+
+message='Lets conquer the world'
+print (message)
+
+notes = 'PYTHON PRACTICE'
+print(notes.title())
+print(notes.upper())
+print(notes.lower())
